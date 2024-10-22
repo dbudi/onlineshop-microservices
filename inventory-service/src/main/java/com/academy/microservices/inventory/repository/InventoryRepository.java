f696e5ac7a734f8c473e0c6dd3201088ef4007c4
22342703 <dui.budi@phincon.com>
2024-10-22 11:06:32 +0700

    initial revision

---
+++ inventory-service/src/main/java/com/academy/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,9 @@
+package com.academy.microservices.inventory.repository;
+
+
+import com.academy.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
