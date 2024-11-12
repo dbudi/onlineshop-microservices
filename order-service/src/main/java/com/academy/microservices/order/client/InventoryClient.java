60c827ae474f44b2c4eaa80cf72f648278853f0d
22342703 <dui.budi@phincon.com>
2024-11-12 15:47:26 +0700

    circuit breaker - resilience4j

--- order-service/src/main/java/com/academy/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/academy/microservices/order/client/InventoryClient.java
@@ -1,14 +1,24 @@
 package com.academy.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "INVENTORY-SERVICE")
 public interface InventoryClient {
+    Logger logger = LoggerFactory.getLogger(InventoryClient.class);
 
-    @RequestMapping(method = RequestMethod.GET, value = "/api/inventory")
+    @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String skuCode, Integer quantity, Throwable throwable){
+        logger.info("Cannot get inventory for skucode {}, failure reason: {}",
+                skuCode, throwable.getMessage());
+        return false;
+    }
 
 }
---
+++ order-service/src/main/java/com/academy/microservices/order/config/RestClientConfig.java
@@ -0,0 +1,36 @@
+package com.academy.microservices.order.config;
+
+import com.academy.microservices.order.client.InventoryClient;
+import org.springframework.boot.web.client.ClientHttpRequestFactories;
+import org.springframework.boot.web.client.ClientHttpRequestFactorySettings;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
+
+import java.time.Duration;
+
+@Configuration
+public class RestClientConfig {
+
+    @Bean
+    public InventoryClient inventoryClient(@LoadBalanced RestClient.Builder restClientBuilder){
+        RestClient restClient = restClientBuilder
+                .baseUrl("http://INVENTORY-SERVICE")
+                .requestFactory(getClientRequestFactory())
+                .build();
+        var restClientAdapter = RestClientAdapter.create(restClient);
+        var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
+        return httpServiceProxyFactory.createClient(InventoryClient.class);
+    }
+
+    private ClientHttpRequestFactory getClientRequestFactory() {
+        ClientHttpRequestFactorySettings clientHttpRequestFactorySettings = ClientHttpRequestFactorySettings.DEFAULTS
+                .withConnectTimeout(Duration.ofSeconds(3))
+                .withReadTimeout(Duration.ofSeconds(3));
+        return ClientHttpRequestFactories.get(clientHttpRequestFactorySettings);
+    }
+}
