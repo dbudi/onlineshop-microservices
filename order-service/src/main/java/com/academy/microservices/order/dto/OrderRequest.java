ed31c71cdc4f13ad2fcede6fe7e53e983189a6c9
22342703 <dui.budi@phincon.com>
2024-11-24 22:47:21 +0700

    event-driven with kafka

--- order-service/src/main/java/com/academy/microservices/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/academy/microservices/order/dto/OrderRequest.java
@@ -2,7 +2,9 @@ package com.academy.microservices.order.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(Long id, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String skuCode, BigDecimal price,
+                           Integer quantity, UserDetails userDetails) {
+    public record UserDetails(String email, String firstName, String lastName) {}
 }
 
 
--- order-service/src/main/java/com/academy/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/academy/microservices/order/service/OrderService.java
@@ -2,14 +2,18 @@ package com.academy.microservices.order.service;
 
 import com.academy.microservices.order.client.InventoryClient;
 import com.academy.microservices.order.dto.OrderRequest;
+import com.academy.microservices.order.event.OrderPlacedEvent;
 import com.academy.microservices.order.model.Order;
 import com.academy.microservices.order.repository.OrderRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 @Transactional
@@ -17,6 +21,7 @@ public class OrderService {
 
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -24,6 +29,16 @@ public class OrderService {
 
         if(isProductInStock) {
             orderRepository.save(order);
+            // Send the message to Kafka Topic
+            OrderPlacedEvent orderPlacedEvent = new OrderPlacedEvent();
+            orderPlacedEvent.setOrderNumber(order.getOrderNumber());
+            orderPlacedEvent.setEmail(orderRequest.userDetails().email());
+            orderPlacedEvent.setFirstName(orderRequest.userDetails().firstName());
+            orderPlacedEvent.setLastName(orderRequest.userDetails().lastName());
+
+            log.info("Start - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
+            kafkaTemplate.send("order-placed", orderPlacedEvent);
+            log.info("End - Sending OrderPlacedEvent {} to Kafka topic order-placed", orderPlacedEvent);
         } else {
             throw new RuntimeException("Product with skuCode="+orderRequest.skuCode()+" is Out of Stock");
         }
